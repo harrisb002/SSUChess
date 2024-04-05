8d06604822086b41ab45b58cbc28e9de611c11b9
Ben Harris <harrisbe@sonoma.edu>
2024-04-04 23:59:57 -0700

    Delete Chessboard

--- Chessboard/src/rules/index.ts
+++
@@ -1,6 +0,0 @@
-// Expose all the rules to combine the rules in imports for referee
-export * from "./PawnRules";
-export * from "./BishopRules";
-export * from "./KnightRules";
-export * from "./KingRules";
-export * from "./RookRules";
