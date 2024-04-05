3f6fe8d8d79aaf92aa76b22ddcaaede6de2ea27f
harrisb002 <harrisbe@sonoma.edu>
2024-04-05 00:01:31 -0700

    Add updated chessboard

---
+++ chessboard/src/Types.ts
@@ -0,0 +1,15 @@
+// Used in piece logic in validating moves
+export enum Side {
+    OPPONENT = 'b',
+    ALLY = 'w',
+  }
+  
+  // Used in piece logic in validating moves
+  export enum PieceType {
+    PAWN = 'pawn',
+    KNIGHT = 'knight',
+    BISHOP = 'bishop',
+    ROOK = 'rook',
+    QUEEN = 'queen',
+    KING = 'king',
+  }
