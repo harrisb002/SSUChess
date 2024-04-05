55c8f742502e335dfcd0ed6ea2a5e61b9b10fc77
harrisb002 <harrisbe@sonoma.edu>
2024-04-04 23:09:24 -0700

    simple navbar

---
+++ chessapp/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+const Navbar = () => {
+  // Get the address of user
+  const address = useAddress();
+
+  return (
+    <div className={styles.navbarContainer}>
+      {/* Conditionally render based on if wallet is connected */}
+      {address && (
+        <>
+          <h1>Chess App</h1>
+          <div className={styles.navbarOptions}>
+            <Link href="/">
+              <p>Dashboard</p>
+            </Link>
+            <Link href="/shop">
+              <p>Puzzles Shop</p>
+            </Link>
+          </div>
+          <div className={styles.navbarOptions}>
+            <ConnectWallet />{" "}
+            {/* UI component from 3rd web that gives functioning connect capabilities*/}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
--- chessapp/pages/_app.tsx
+++ chessapp/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider, embeddedWallet, smartWallet } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  embeddedWallet,
+  smartWallet,
+} from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -16,9 +21,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         smartWallet(embeddedWallet(), {
           factoryAddress: "",
           gasless: true,
-        })
+        }),
       ]}
     >
+      <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
